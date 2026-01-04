c06b48f75f2c5b2e3845e95b96b48a7a83aae8a4
SterlinkIng89 <99hongo@live.com.mx>
2026-01-03 22:26:37 -0600

    S1 - 1

--- ejercicios-algoritmos/sesion-1/isograma/index.ts
+++ ejercicios-algoritmos/sesion-1/isograma/index.ts
@@ -1,4 +1,18 @@
 export default function esIsograma(string: string): boolean {
-  // TODO: implement
-  return false;
+  if (string.length === 0) {
+    return true;
+  }
+
+  if (string.split(" ").length > 1) {
+    return false;
+  }
+
+  const cleanString = string
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLocaleLowerCase();
+
+  return new Set(cleanString).size === cleanString.length;
 }
+
+console.log("result:", esIsograma("Murciélago"));
