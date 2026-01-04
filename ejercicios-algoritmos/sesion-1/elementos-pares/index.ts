6b429c68bcc76adbf69d83d6304f86b0381eacb8
SterlinkIng89 <99hongo@live.com.mx>
2026-01-04 01:02:43 -0600

    S1 - 4

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
@@ -1,4 +1,19 @@
 export default function filtrarPares(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+  let result: unknown[] = [];
+  let map = new Map<unknown, number>();
+
+  for (const item of array) {
+    const count = map.get(item) || 0;
+    map.set(item, count + 1);
+  }
+
+  map.forEach((value, key) => {
+    if (value % 2 === 0) {
+      result.push(key);
+    }
+  });
+
+  return result;
 }
+
+console.log(filtrarPares([1, 2, 3, 1, 2]));
