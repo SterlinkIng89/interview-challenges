a812ccb49e768ff77524de7b41ba1a3942b6ce82
SterlinkIng89 <99hongo@live.com.mx>
2026-01-03 22:39:55 -0600

    S1 - 2

--- ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
+++ ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
@@ -1,4 +1,14 @@
 export default function letrasPorNumeros(string: string): string {
-  // TODO: implement
-  return "";
+  return string
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLocaleLowerCase()
+    .replace(/\s/g, "")
+    .split("")
+    .map((char) => char.charCodeAt(0) - 96)
+    .join(" ");
 }
+
+// Just clean up the string an then split, map each char, join and return
+
+console.log(letrasPorNumeros("abc def"));
